23ff1fda4e2cd39730713882a778cf48027ec2fb
Meghan Flynn <mflynn333@gmail.com>
2020-03-02 21:31:47 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -10,4 +10,11 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   end
 end
 
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
+
+
+
 end
