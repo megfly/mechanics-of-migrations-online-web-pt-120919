c14dc61752dfe707343e066f3b028c836031bf19
Meghan Flynn <mflynn333@gmail.com>
2020-03-02 21:45:08 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -8,18 +8,6 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   class CreateArtists < ActiveRecord::Migration[5.2]
   def change
   end
-  
-sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS artists (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  genre TEXT,
-  age INTEGER,
-  hometown TEXT
-  )
-SQL
- 
-ActiveRecord::Base.connection.execute(sql)
 
 def change
   create_table :artists do |t|
