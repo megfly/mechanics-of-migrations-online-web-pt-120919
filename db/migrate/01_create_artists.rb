689d21c05b36fc4747b96b8c089962464ebf3a80
Meghan Flynn <mflynn333@gmail.com>
2020-03-02 21:43:15 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -8,12 +8,7 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   class CreateArtists < ActiveRecord::Migration[5.2]
   def change
   end
-
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/artists.sqlite"
-)
-
+  
 sql = <<-SQL
   CREATE TABLE IF NOT EXISTS artists (
   id INTEGER PRIMARY KEY,
