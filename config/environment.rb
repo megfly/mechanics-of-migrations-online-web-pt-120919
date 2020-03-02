f7d96f4063f1de5517e339c094aa7b62d617b2f9
Meghan Flynn <mflynn333@gmail.com>
2020-03-02 21:51:25 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -9,8 +9,8 @@ require 'bundler/setup'
 Bundler.require
 
 ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/artists.sqlite"
-)
+    :adapter => "sqlite3",
+    :database => "db/artists.sqlite"
+  )
 
 require_relative "../artist.rb"
